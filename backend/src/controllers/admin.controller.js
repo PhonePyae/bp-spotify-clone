bbeb52a1e9619cde20f76f53fc582751d17d7446
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-04-10 00:42:43 +0800

    Cloudinary Setup & Create Song Functionality

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,3 +1,51 @@
-export const getAdmin = (req, res) => {
-    res.send("Admin route with GET method");  
+import { Song } from "../models/song.model.js";
+import { Album } from "../models/album.model.js";
+import cloudinary   from "../lib/cloudinary.js";
+
+//Helper function to cloudinary upload
+const uploadToCloudinary = async (file) => {
+    try{
+        const result = await cloudinary.uploader.upload(file.tempFilePath, {
+            resource_type: "auto",
+        });
+        return result.secure_url;
+    }
+}
+
+export const createSong = async (req, res, next  ) => {
+    try{
+        if(!req.files || !req.files.audioFile || !req.files.imageFile  ){
+            return res.status(400).json({message: "Please upload all files"});
+        }
+
+        const {title, artist, albumId, duration} = req.body;
+        const audioFile = req.files.audioFile;
+        const imageFile = req.files.imageFile;
+
+        const audioUrl = await uploadToCloudinary(audioFile);
+        const imageUrl = await uploadToCloudinary(imageFile);
+
+        const song = new Song({
+            title,
+            artist,
+            audioUrl,
+            imageUrl,
+            duration,
+            albumId: albumId || null,  
+        }); 
+
+        await song.save();
+
+        // if song blogs to an album, add the song ID to the album's songs array
+        if(albumId){
+            await Album.findByIdAndUpdate(albumId, {
+                $push: { songs: song._id } // Add song ID to the album's songs array
+            });
+        }
+
+        res.status(201).json({song});
+    }catch(error){
+        console.log('Error creating song:', error);
+        next(error); 
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv'; 
 
 import { connectDB } from './lib/db.js';
-import { clerkMiddleware } from '@clerk/express'; 
+import { clerkMiddleware } from '@clerk/express';
+import fileupload from 'express-fileupload';
+import path from 'path';  
   
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
@@ -15,14 +17,28 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve(); // Get the current directory name
 
 app.use(express.json()); // Middleware to parse JSON request bodies
-app.use(clerkMiddleware()); // Middleware to handle. This will add auth to req obj => req.auth.userId 
+app.use(clerkMiddleware()); // This  will add auth to req obj => req.auth.userId
+app.use(fileupload({
+  useTempFiles: true, // Store files in a temporary directory
+  tempFileDir: path.join(__dirname, 'tmp'), // Directory for temporary files
+  createParentPath: true, // Create parent directory if it doesn't exist
+  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10MB
+})); // Middleware to handle file uploads
+
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);  
+app.use('/api/admin', adminRoutes);
+app.use('/api/songs', songRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statRoutes);
+
+// Error handling middleware 
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: process.env.NODE_ENV === 'production' ? 'Internal Server Error' : err.message });
+});
 
 app.listen(PORT, () => {
   console.log('Server is running on port 5009 ');
