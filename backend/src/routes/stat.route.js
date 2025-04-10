3bad3aa12a57056efd6b9fd5e893dcb0c97dfd61
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-04-11 00:42:29 +0800

    Stat Route & Controller

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { getStats } from "../controllers/stat.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-res.send("Stat route with GET method");  
-});
+// @desc    Get all stats
+// @route   GET /api/stats
+// @access  Private/Admin
+router.get("/", protectRoute, requireAdmin, getStats);
 
 export default router;
