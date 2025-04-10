c90fa45a89257e24931718eec9c7eb1ae8dd33e1
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-04-10 23:57:41 +0800

    Album Routes & Controllers

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { getAllAlbums, getAlbumById } from "../controllers/album.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-res.send("Album route with GET method");  
-});
+// @desc    Get all albums
+// @route   GET /api/albums
+// @access  Public
+router.get("/", getAllAlbums); // Get all albums
+
+// @desc    Get album by ID
+// @route   GET /api/albums/:id
+// @access  Public
+router.get("/:id", getAlbumById); // Get album by ID 
 
 export default router;
