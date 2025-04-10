f52dc5d5074c562511de726fa704ca84070decf6
Phone Pyae <phonepyaethantzin2000@gmail.com>
2025-04-11 00:29:42 +0800

    User Route & Controller

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("User route with GET method");  
-});
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private
+router.get("/", protectRoute, getAllUsers);
+
+// To Do: getMessages
 
 export default router;
